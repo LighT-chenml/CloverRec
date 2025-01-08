33b4936980746502a01968241c329cbcd90bcf09
LighT-chenml <chenml@hust.edu.cn>
2025-01-08 16:19:49 +0800

    update

--- local_emb/benchmark.py
+++ local_emb/benchmark.py
@@ -2,23 +2,6 @@ import subprocess
 import argparse
 import asyncio
 
-def start_GPU_server(batch_size):
-    commands = 'ssh cml@192.168.123.4 "source ~/.bashrc; cd /home/cml/CloverRec/local_emb; chmod +x ./benchmark_model.sh ; ./benchmark_model.sh 32" '
-    # subprocess.run(commands, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    result = subprocess.run(commands, shell=True, capture_output=True, text=True)
-    print(f"Output: {result.stdout}")
-    print(f"Error: {result.stderr}")
-
-def close_GPU_server():
-    proc_name="CloverRec_GPU_server"
-    command = f"pkill {proc_name}"
-
-    GPU_server = "cml@192.168.123.4" # PM2
-    ssh_command = f"ssh {GPU_server} {command}"
-    result = subprocess.run(ssh_command, shell=True, capture_output=True, text=True)
-    print(f"Output: {result.stdout}")
-    print(f"Error: {result.stderr}")
-
 def start_coordinator(batch_size):
     command = f"./benchmark_coordinator.sh {batch_size}"
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
@@ -33,11 +16,9 @@ def run():
     # model related parameters
     parser.add_argument("--arch-batch-size", type=int, default=2)
 
-    batch_sizes = [32]
+    batch_sizes = [1, 2, 4, 8, 16, 32, 64, 128, 256]
     for batch_size in batch_sizes:
-        start_GPU_server(batch_size)
-        # start_coordinator(batch_size)
-        # close_GPU_server()
+        start_coordinator(batch_size)
 
 if __name__ == "__main__":
     run()
--- local_emb/dlrm_emb_storage.py
+++ local_emb/dlrm_emb_storage.py
@@ -4,15 +4,22 @@ import numpy as np
 # pytorch
 import torch
 
+import time
+
 class EmbStorage():
     def __init__(self, m, ln):
         self.emb_l = []
         for i in range(0, ln.size):
             n = ln[i]
             # initialize embeddings
-            W = np.random.uniform(
-                low=-np.sqrt(1 / n), high=np.sqrt(1 / n), size=(n, m)
-            ).astype(np.float32)
+            # W = np.random.uniform(
+            #     low=-np.sqrt(1 / n), high=np.sqrt(1 / n), size=(n, m)
+            # ).astype(np.float32)
+            
+            low = -np.sqrt(1 / n)
+            high = np.sqrt(1 / n)
+            W = low + torch.rand(n, m ,dtype=torch.float32) * (high - low)
+            
             self.emb_l.append(W)
     
     def apply_emb(self, lS_o, lS_i):
@@ -22,7 +29,7 @@ class EmbStorage():
         #   corresponding to a single lookup
         # 2. for each embedding the lookups are further organized into a batch
         # 3. for a list of embedding tables there is a list of batched lookups
-
+        
         ly = []
         for k, sparse_index_group_batch in enumerate(lS_i):
             sparse_offset_group_batch = lS_o[k]
@@ -38,18 +45,43 @@ class EmbStorage():
             evs = []
             
             for i in range(batch_size):
-                if i != batch_size - 1:
-                    index = sparse_index_group_batch[sparse_offset_group_batch[i]:sparse_offset_group_batch[i+1]]
-                else:
-                    index = sparse_index_group_batch[sparse_offset_group_batch[i]:]
-                ev = E[index]
+                
+                # start_time = time.time()
+                
+                start = sparse_offset_group_batch[i]
+                end = sparse_offset_group_batch[i + 1] if i + 1 < batch_size else len(sparse_index_group_batch)
+                
+                # end_time = time.time()
+                # start_time = time.time()
+                
+                ev = E[sparse_index_group_batch[start:end]]
+                
+                # end_time = time.time()
+                
+                # total_time = end_time - start_time
+                # total_time *= 1000
+                # print("total_lookup_time (ms): " + f"{total_time * len(lS_i) * batch_size}")
+                
+                # start_time = time.time()
                 
                 # mode = "sum"
-                ret = sum(ev)
-                evs.append(ret)
+                # evs.append(sum(ev))
+                evs.append(ev.sum(dim=0))
+                
+                # end_time = time.time()
+                
+                # total_time = end_time - start_time
+                # total_time *= 1000
+                # print("total_cal_time (ms): " + f"{total_time * len(lS_i) * batch_size}")
+            
+            # start_time = time.time()
             
             V = torch.tensor(np.array(evs))
             ly.append(V)
+            
+            # end_time = time.time()
+            # total_time = end_time - start_time
+            # total_time *= 1000
+            # print("total_time (ms): " + f"{total_time * len(lS_i)}")
 
-        # print(ly)
         return ly
